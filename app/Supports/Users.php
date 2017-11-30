d1b79f927d27cbda698b3a1de35305f36e9cce0e
zhanpei <zhangzhanpei@gmail.com>
2017-11-30 16:43:23 +0800

    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

---
+++ app/Supports/Users.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Supports;
+
+class Users
+{
+    public function find(int $userId) {
+        
+    }
+}
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -4,16 +4,34 @@ use PHPUnit\Framework\TestCase;
 
 class MathTest extends TestCase
 {
+    protected $m;
+
+    public function setUp()
+    {
+        $this->m = new Math();
+    }
+
     public function testAdd()
     {
-        $m = new Math();
-        $this->assertEquals(3, $m->add(1, 2));
+        $this->assertEquals(3, $this->m->add(1, 2));
     }
 
     public function testDevide()
     {
-        $m = new Math();
-        $this->assertEquals(-1, $m->devide(1, 0));
-        $this->assertEquals(3, $m->devide(6, 2));
+        $this->assertEquals(-1, $this->m->devide(1, 0));
+        $this->assertEquals(3, $this->m->devide(6, 2));
+    }
+
+    public function additionDataProvider()
+    {
+        return [[1, 2, 3], [1, -1, 0]];
+    }
+
+    /**
+     * @dataProvider additionDataProvider
+     */
+    public function testAdd2($a, $b, $expected)
+    {
+        $this->assertEquals($expected, $this->m->add($a, $b));
     }
 }
